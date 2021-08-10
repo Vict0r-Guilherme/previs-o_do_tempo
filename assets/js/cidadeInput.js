42cf90a87e9dd4ee3b165c155ab97b6c3747bd32
Victor Guilherme <87831118+Vict0r-Guilherme@users.noreply.github.com>
2021-08-10 01:11:41 -0300

    Add files via upload

---
+++ assets/js/cidadeInput.js
@@ -0,0 +1,36 @@
+import {criarCards} from './main.js';
+import {criarClima} from './main.js'
+
+const pegarLocalizacao = () =>{
+    const inputCidade = document.querySelector("[data-input]");
+    if(inputCidade.value === ''){
+        inputCidade.classList.add('form__input--invalido');
+        alert("campo vazio")
+        const cidade = "recife"
+        return cidade;
+    }
+    else{
+        inputCidade.classList.remove('form__input--invalido');
+        const cidade = inputCidade.value.replace(/ /g, "_");
+        inputCidade.value = ''
+        return cidade;
+    }
+}
+
+const btn = document.querySelector("[data-submit]");
+btn.addEventListener('click',async (evento) =>{
+    evento.preventDefault();
+
+    const loader = document.querySelector("[data-loader]");
+    const cidade = pegarLocalizacao();
+
+    fetch(`https://api.hgbrasil.com/weather?format=json-cors&key=3ce1acab&city_name=${cidade}&array_limit=7&fields=only_results,city_name,city,date,temp,condition_slug,description,forecast,weekday,condition,max,min`)
+    .then(response =>{response.json()
+            .then(dados => {
+                criarCards(dados)
+                criarClima(dados)
+                loader.classList.add('loader-off')
+            })
+    })
+    loader.classList.remove('loader-off')
+})
---
+++ assets/js/main.js
@@ -0,0 +1,66 @@
+const pegarLocalAtual = () =>{
+    if('geolocation' in navigator){
+        navigator.geolocation.getCurrentPosition(function(position){
+            consultaApigeolocation(position.coords.latitude, position.coords.longitude);
+           
+        }, 
+        consultaApipadrao())
+    }
+    else{
+        consultaApipadrao()
+    }
+    
+}
+
+const consultaApipadrao = () => {
+    fetch(`https://api.hgbrasil.com/weather?format=json-cors&key=3ce1acab&city_name=recife&array_limit=7&fields=only_results,city_name,city,date,temp,condition_slug,description,forecast,weekday,condition,max,min`)
+            .then(response => {
+                response.json().then(dados =>{
+                    criarClima(dados);
+                    criarCards(dados);
+                })
+        })
+}
+
+const consultaApigeolocation = (latitude, longitude) =>{
+    fetch(`https://api.hgbrasil.com/weather?format=json-cors&key=3ce1acab&lat=${latitude}&lon=${longitude}&array_limit=7&fields=only_results,city_name,city,date,temp,condition_slug,description,forecast,weekday,condition,max,min`)
+    .then(response => {
+        response.json().then(dados =>{
+            criarClima(dados);
+            criarCards(dados);
+        })
+    })
+}
+
+export const criarClima = (dados) =>{
+    const cardClima = document.querySelector(".principal__clima");
+    const climaAtual = `<img src="./assets/img/${dados.condition_slug}.png" alt="Clima" class="principal__img">
+        <h1 class="principal__descricao principal__descricao--temp" data-temp>${dados.temp}<span>°C</span></h1>
+        <span class="principal__descricao principal__descricao--condition" data-clima>${dados.description}</span>
+        <span class="principal__descricao" data-dia>hoje - ${dados.forecast[0].weekday}, ${dados.forecast[0].date}</span>
+        <span class="principal__descricao" data-local>${dados.city}</span>`
+    cardClima.innerHTML = climaAtual;
+}
+
+export const criarCards = (dados) =>{
+    const semana = document.querySelector("[data-clima-semana]")
+    semana.innerHTML = '';
+    const climaSemana = dados.forecast;
+    climaSemana.forEach( dia =>{
+        const divGroup = document.createElement("div");
+        divGroup.classList.add("clima-semana__card");
+        divGroup.innerHTML = `
+            <div class="clima-semana__card">
+                <span class="card__descricao">${dia.weekday}, ${dia.date}</span>
+                <img src="./assets/img/${dia.condition}.png" class="card__img">
+                <span class="card__descricao">${dia.description}</span>
+                <div>
+                    <span class="card__descricao">${dia.max}°C</span>
+                    <span class="card__descricao">${dia.min}°C</span>
+                </div>
+            </div>`;
+        semana.appendChild(divGroup)
+    })
+}
+
+pegarLocalAtual()
